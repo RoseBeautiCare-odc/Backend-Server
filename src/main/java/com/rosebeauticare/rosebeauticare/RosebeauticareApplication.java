ada5913e2c8ebf9ce16d384197c1c729c96854b1
Thilak Raj <thilakofficials@gmail.com>
2025-06-10 15:00:22 +0530

    created new branch

---
+++ src/main/java/com/rosebeauticare/rosebeauticare/RosebeauticareApplication.java
@@ -0,0 +1,13 @@
+package com.rosebeauticare.rosebeauticare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosebeauticareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosebeauticareApplication.class, args);
+	}
+
+}
